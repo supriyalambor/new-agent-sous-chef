83e363e13bf0bf8298af96ecf768856b1b81f3e7
Supriya Lambor <supriyalambor@gmail.com>
2026-05-21 21:47:41 +0530

    commit

--- backend/main.py
+++ backend/main.py
@@ -15,6 +15,7 @@ app = FastAPI(title="Sous Chef API")
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],
+    allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
